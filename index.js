f59f39be99fe677a15b64f53bacec897b4a0dc50
unknown <prathiba.byadarahallikeshava@mavs.uta.edu>
2015-10-01 18:36:26 -0500

    commit2

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/getItems',function(request, response){
+	response.render('pages/items');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
