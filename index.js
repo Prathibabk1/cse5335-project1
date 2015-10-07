66d7d9a7800f9381c29230e23ea935108555080e
unknown <prathiba.byadarahallikeshava@mavs.uta.edu>
2015-10-06 19:52:47 -0500

    edit3

--- index.js
+++ index.js
@@ -20,48 +20,48 @@ var obj=[
             "capital": "Autsin",
             "location":"Austin",
             "latitude":30.2500, 
-            "longitude":97.7500
+            "longitude":-97.7500
         },
         {
            "state":"Arizona", 
             "capital": "Phoenix",
             "location":"Phoenix",
             "latitude": 33.4500, 
-            "longitude": 112.0667
+            "longitude":-112.0667
         },
         {
             "state": "Atlanta", 
             "capital" :"Georgia",
             "location":"Georgia",
             "latitude": 32.9605,
-            "longitude": 83.1132
+            "longitude":-83.1132
         },
         {
             "state":"Arkansas" ,
             "capital":"Little Rock",
             "location":"Little Rock",
             "latitude": 34.7361,
-            "longitude": 92.3311
+            "longitude":-92.3311
         },
          {
             "state":"Ohio",
             "capital" : "Columbus",
              "location":"Columbus",
              "latitude": 39.9833, 
-             "longitude": 82.9833
+             "longitude":-82.9833
         },
         {
             "state":"Kentucky" ,
             "capital":"Frankfort",
             "location":"Frankfort",
             "latitude":38.1970, 
-            "longitude": 84.8630
+            "longitude":-84.8630
         },
         {
             "state":"Colorado",
             "capital" : "Denver",
             "location":"Denver",
-            "latitude": 39.7392,
+            "latitude":-39.7392,
             "longitude":104.9903
         },
         {
@@ -69,21 +69,21 @@ var obj=[
             "capital" :"Raleigh",
             "location":'Raleigh',
             "latitude": 35.7806,
-            "longitude":78.6389
+            "longitude":-78.6389
         },
          {
             "state":"South Carolina",
              "capital" : "Columbia",
              "location":"Columbia",
              "latitude": 34.0298,
-             "longitude": 80.8966
+             "longitude":-80.8966
         },
         {
             "state":"Pennsylvania",
             "capital":"Harrisburg",
             "location":"Harrisburg",
             "latitude":40.2697, 
-            "longitude": 76.8756
+            "longitude":-76.8756
         }
     ];
 app.get('/getItems', function(req, res) {
